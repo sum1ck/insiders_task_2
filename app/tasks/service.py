2b29165a481bdb232d20dda131e0188ab84fa488
sum1ck <stanislavpetruk@gmail.com>
2025-06-30 16:42:32 +0300

    fix celery task

--- app/tasks/service.py
+++ app/tasks/service.py
@@ -9,9 +9,14 @@ from tasks.models import Task, TaskStatus
 from core.config import settings
 from sqlalchemy import select
 import asyncio
+from sqlalchemy import create_engine
 
 engine = create_async_engine(settings.DATABASE_URL, echo=False)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+SYNC_DATABASE_URL = settings.DATABASE_URL.replace("+asyncpg", "")
+engine = create_engine(SYNC_DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
 
 class TaskService:
     @staticmethod
@@ -40,18 +45,13 @@ class TaskService:
 
 @celery_app.task(name="tasks.service.mark_overdue_tasks")
 def mark_overdue_tasks():
-    async def _mark():
-        async with AsyncSessionLocal() as session:
-            now = datetime.datetime.now(datetime.timezone.utc)
-            result = await session.execute(
-                select(Task).where(
-                    Task.status == TaskStatus.pending,
-                    Task.due_date != None,
-                    Task.due_date < now
-                )
-            )
-            tasks = result.scalars().all()
-            for task in tasks:
-                task.status = TaskStatus.overdue
-            await session.commit()
-    asyncio.run(_mark()) 
+    with SessionLocal() as session:
+        now = datetime.datetime.now(datetime.timezone.utc)
+        tasks = session.query(Task).filter(
+            Task.status == TaskStatus.pending,
+            Task.due_date != None,
+            Task.due_date < now
+        ).all()
+        for task in tasks:
+            task.status = TaskStatus.overdue
+        session.commit() 
